d22a46fdd7fc3c794b67f6d8b56cfeb2852f3fc7
Hakimi Mdnoor <hakimimdnoor90@gmail.com>
2023-03-01 12:08:04 +0800

    code analyst

--- lib/readFirebase/read.dart
+++ lib/readFirebase/read.dart
@@ -1,9 +1,6 @@
 import 'package:aqwise_stripe_payment/readFirebase/sample.model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/async.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ReadFromFirebase extends StatefulWidget {
   const ReadFromFirebase({super.key});
