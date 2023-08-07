1c13d5fb48d7697f4f228dd87cf531dbe61eb561
tomyamGTX <hakimimdnoor90@gmail.com>
2023-08-07 15:30:06 +0800

    onboarding

--- lib/.env.dart
+++ lib/.env.dart
@@ -2,3 +2,10 @@ const String StripePublishableKey =
     'pk_test_51KUoMmH8qbOVfW8TpDMbk4SQbcncSFc9y8sEOE2iHtIvm7nKd0MdQrs8SZp49jY5ZiDKI2qYLS276RcvmKfIe55Z00yIGfZX2g';
 const String StripeSecretKey =
     'sk_test_51KUoMmH8qbOVfW8To6sFBH3Mg7yXUzdBXUw6VOM7dy0I9LA8JqaTFIS44SenWfY61DrKSvgWmm4Pi05OZWed7SRn00GI4jxA9V';
+
+// <key>CLIENT_ID</key>
+// <string>525737552936-tk5uov2tnuf2u3cfud1ga9hm4ghsjiak.apps.googleusercontent.com</string>
+// <key>REVERSED_CLIENT_ID</key>
+// <string>com.googleusercontent.apps.525737552936-tk5uov2tnuf2u3cfud1ga9hm4ghsjiak</string>
+// <key>API_KEY</key>
+// <string>AIzaSyD2s5gGIo8IGJAZHTYVSkK3KdruBvyqYnk</string>
